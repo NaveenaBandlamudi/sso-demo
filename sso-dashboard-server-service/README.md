4ed53521cb40652af6ac56f6761ba07335173a0d
NaveenaBandlamudi <naveena.bandlamudi@msr-it.com>
2019-08-20 17:55:08 +0530

    first commit

---
+++ sso-dashboard-server-service/README.md
@@ -0,0 +1,7 @@
+127.0.0.1 sso-server-service.cloud.naveena.com
+
+127.0.0.1 sso-server-client.cloud.naveena.com
+
+127.0.0.1 sso-dashboard-client.cloud.naveena.com
+
+127.0.0.1 sso-dashboard-service.cloud.naveena.com
---
+++ sso-server-server-service/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cors = require("cors");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 8020;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// const app = require("./app");
+// const PORT = 8020;
+
+// app.listen(PORT, () => {
+//   console.info(`sso-server listening on port ${PORT}`);
+// });
